6918877ce0cb3c72757d8563ee955afe957ab66d
erikthomasjohnson <erikjohnsoncomposer@gmail.com>
2016-09-28 08:24:38 -0500

    individualized health effects

--- roleplay.js
+++ roleplay.js
@@ -81,6 +81,22 @@ function GetFinalGold()
 	this.finalgold = this.gold * this.health + 1004;
 }
 
+function personHealthOption01(){
+	if (person.name == "Scuba Larry"){person.health -= (Math.ceil(Math.random()*10) + 10);}
+	if (person.name == "Attorney Jimmy"){person.health -= (Math.ceil(Math.random()*10) + 15);}
+	if (person.name == "Sir Jon"){person.health -= (Math.ceil(Math.random()*10) + 5);}
+}
+function personHealthOption02(){
+	if (person.name == "Scuba Larry"){person.health -= (Math.ceil(Math.random()*10) + 15);}
+	if (person.name == "Attorney Jimmy"){person.health -= (Math.ceil(Math.random()*10) + 5);}
+	if (person.name == "Sir Jon"){person.health -= (Math.ceil(Math.random()*10) + 10);}
+}
+function personHealthOption03(){
+	if (person.name == "Scuba Larry"){person.health -= (Math.ceil(Math.random()*10) + 5);}
+	if (person.name == "Attorney Jimmy"){person.health -= (Math.ceil(Math.random()*10) + 10);}
+	if (person.name == "Sir Jon"){person.health -= (Math.ceil(Math.random()*10) + 15);}
+}
+
 function page00Button(){
 	if (document.getElementById("radio-1").checked) {
 		person = new Person("Sir Jon");
@@ -108,46 +124,51 @@ function page01Button(){
 	if (document.getElementById("radio-5").checked) {
 		value = document.getElementById("radio-5").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-6").checked) {
 		value = document.getElementById("radio-6").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-7").checked) {
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-8").checked) {
 		value = document.getElementById("radio-8").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=(Math.ceil(Math.random()*15)+5);
 
 	document.getElementById("gold").innerHTML = person.gold;
 	document.getElementById("health").innerHTML = person.health;
 	document.getElementById("page01").style.display = "none";
 	document.getElementById("page02").style.display = "block";
 }
-
 function page02Button()
 {
 	var value = 0;
 	if (document.getElementById("radio-9").checked) {
 		value = document.getElementById("radio-9").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-10").checked) {
 		value = document.getElementById("radio-10").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-11").checked) {
 		value = document.getElementById("radio-11").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-12").checked) {
 		value = document.getElementById("radio-12").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=Math.ceil(Math.random()*20);
 	document.getElementById("gold").innerHTML = person.gold;
 	document.getElementById("health").innerHTML = person.health;
 	document.getElementById("page02").style.display = "none";
@@ -160,20 +181,23 @@ function page03Button()
 	if (document.getElementById("radio-13").checked) {
 		value = document.getElementById("radio-13").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-14").checked) {
 		value = document.getElementById("radio-14").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-15").checked) {
 		value = document.getElementById("radio-15").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-16").checked) {
 		value = document.getElementById("radio-16").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=(Math.ceil(Math.random()*15)+5);
 	document.getElementById("gold").innerHTML = person.gold;
 	document.getElementById("health").innerHTML = person.health;
 	document.getElementById("page03").style.display = "none";
@@ -186,20 +210,23 @@ function page04Button()
 	if (document.getElementById("radio-17").checked) {
 		value = document.getElementById("radio-17").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-17").checked) {
 		value = document.getElementById("radio-17").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-18").checked) {
 		value = document.getElementById("radio-18").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-19").checked) {
 		value = document.getElementById("radio-19").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=Math.ceil(Math.random()*20);
 	document.getElementById("gold").innerHTML = person.gold;
 	document.getElementById("health").innerHTML = person.health;
 	document.getElementById("page04").style.display = "none";
@@ -212,24 +239,40 @@ function page05Button()
 	if (document.getElementById("radio-21").checked) {
 		value = document.getElementById("radio-20").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-22").checked) {
 		value = document.getElementById("radio-22").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-23").checked) {
 		value = document.getElementById("radio-23").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-24").checked) {
 		value = document.getElementById("radio-24").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=(Math.ceil(Math.random()*15)+5);
+	GetFinalGold.call(person);
+	document.getElementById("player2").innerHTML = person.name;
+	document.getElementById("player3").innerHTML = person.name;
+	document.getElementById("finalgold").innerHTML = person.finalgold;
+	document.getElementById("finalgold2").innerHTML = person.finalgold;
 	document.getElementById("gold").innerHTML = person.gold;
+	document.getElementById("gold2").innerHTML = person.gold;
+	document.getElementById("gold3").innerHTML = person.gold;
 	document.getElementById("health").innerHTML = person.health;
+	document.getElementById("health2").innerHTML = person.health;
+	document.getElementById("health3").innerHTML = person.health;
 	document.getElementById("page05").style.display = "none";
 	document.getElementById("page06").style.display = "block";
+	if (person.health<0)
+	{
+		endGame();
+	}
 }
 
 function page06Button()
@@ -238,23 +281,22 @@ function page06Button()
 	if (document.getElementById("radio-25").checked) {
 		value = document.getElementById("radio-25").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-26").checked) {
 		value = document.getElementById("radio-26").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-27").checked) {
 		value = document.getElementById("radio-27").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-28").checked) {
 		value = document.getElementById("radio-28").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
-	}
-	person.health -=Math.ceil(Math.random()*20);
-	if (person.health<0)
-	{
-		endGame();
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
 	GetFinalGold.call(person);
 	document.getElementById("player2").innerHTML = person.name;
@@ -269,6 +311,10 @@ function page06Button()
 	document.getElementById("health3").innerHTML = person.health;
 	document.getElementById("page06").style.display = "none";
 	document.getElementById("page07").style.display = "block";
+	if (person.health<0)
+	{
+		endGame();
+	}
 }
 
 function page07Button()
@@ -277,20 +323,23 @@ function page07Button()
 	if (document.getElementById("radio-29").checked) {
 		value = document.getElementById("radio-29").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-30").checked) {
 		value = document.getElementById("radio-30").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-31").checked) {
 		value = document.getElementById("radio-31").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-32").checked) {
 		value = document.getElementById("radio-32").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=(Math.ceil(Math.random()*15)+5);
 	GetFinalGold.call(person);
 	document.getElementById("player2").innerHTML = person.name;
 	document.getElementById("player3").innerHTML = person.name;
@@ -316,20 +365,23 @@ function page08Button()
 	if (document.getElementById("radio-33").checked) {
 		value = document.getElementById("radio-33").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption01();
 	}
 	if (document.getElementById("radio-34").checked) {
 		value = document.getElementById("radio-34").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption02();
 	}
 	if (document.getElementById("radio-35").checked) {
 		value = document.getElementById("radio-35").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		personHealthOption03();
 	}
 	if (document.getElementById("radio-36").checked) {
 		value = document.getElementById("radio-36").value;
 		person.gold+=(parseInt(value)+Math.ceil(Math.random()*10));
+		person.health -= (Math.ceil(Math.random()*10) + 5);
 	}
-	person.health -=Math.ceil(Math.random()*20);
 	GetFinalGold.call(person);
 	document.getElementById("player2").innerHTML = person.name;
 	document.getElementById("player3").innerHTML = person.name;
